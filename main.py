1543a7e37e6c3c8ba5577381e0efe856ea2d6ef7
saron dubale <127416435+sarondubale@users.noreply.github.com>
2023-03-11 03:58:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from random import randint
+
+board = []
+
+# Creating a board
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+# Printing out the board on screen
+def print_board(board):
+  for row in board:
+    print (" ".join(row))
+
+print_board(board)
+
+# Get a random row number
+def random_row(board):
+  return randint(0, len(board[0]) - 1)
+  
+
+# Get a random column number
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+  
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+#Loop everything after this. This should be done at the very end
+# Print the turn the player is on
+for turn in range(4):
+  print ("Turn", turn+1)
+  guess_row = int(input("Guess Row: "))
+  guess_col = int(input("Guess Col: "))
+  
+  # Write your if else code below!
+  # Write your code below!
+  if (guess_row == ship_row and guess_col == ship_col):
+    print ("Congratulations! You sank my battleship!")
+    break
+  else:
+    if(guess_row not in range(5) or guess_col not in range(5)):
+      print("Oops, that's not even in the ocean..")
+    elif board[guess_row][guess_col] == 'X':
+      print("You guessed that one already")
+    else:
+      print("You missed my battleship")
+      board[guess_row][guess_col] = "X"
+      print_board(board)
+    if(turn == 3):
+      print("Game Over")
